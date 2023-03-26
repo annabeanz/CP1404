1561e5f5a2393225de1861e25bd46c92bd88b43e
annabeanz <anastasia.ereme0307@gmail.com>
2023-03-26 18:52:46 +0800

    Refactor password check program to use functions

--- Prac_02/password_stars.py
+++ Prac_02/password_stars.py
@@ -2,10 +2,22 @@
 Password checker
 
 """
+def main():
+    key = get_password()
+    out_stars(key)
 
-key = input("Choose your password: ")
-while len(key) < 10:
-    print("Please input a password at least 10 characters in length")
+
+def out_stars(key):
+    for i in range(len(key)):
+        print("*", end="")
+
+
+def get_password():
     key = input("Choose your password: ")
-for i in range(len(key)):
-    print("*", end="")
+    while len(key) < 10:
+        print("Please input a password at least 10 characters in length")
+        key = input("Choose your password: ")
+    return key
+
+
+main()
